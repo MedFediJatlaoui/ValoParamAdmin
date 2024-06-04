cce107e665dbf82dda6d58cecba20408b7c5d031
mohamedfedijatlaoui <mohamed-fedi.jatlaoui@talan.com>
2024-06-04 15:00:14 +0100

    dir

--- Backend/src/test/java/com/talan/adminmodule/service/impl/ParamTableServiceImplTest.java
+++ Backend/src/test/java/com/talan/adminmodule/service/impl/ParamTableServiceImplTest.java
@@ -91,7 +91,7 @@ void testDeleteCascade(){
 
     DeleteRequest cascade = new DeleteRequest("country","1");
        ResponseDto response= paramTableService.deletecascade(cascade);
-    assertEquals("Cascade deletion succeeded",response.getSuccess());
+    assertEquals("Deletion will be executed at 8 AM",response.getSuccess());
     List<DeleteRequest> deletedd = new ArrayList<>();
     paramTableService.checkReferencedRecursive(cascade,deletedd);
 //    assertEquals(3,deletedd.size());
