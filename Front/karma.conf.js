37e2cbd82ce1b251605cf3717fdc39a80f3a4a8d
mohamedfedijatlaoui <mohamed-fedi.jatlaoui@talan.com>
2024-06-05 15:45:59 +0100

    pipelinefront

--- Front/karma.conf.js
+++ Front/karma.conf.js
@@ -6,7 +6,6 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-jasmine-html-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-sonarqube-reporter'),
@@ -41,7 +40,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
